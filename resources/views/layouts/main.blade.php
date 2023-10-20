2410ed667ec352189a7baa7dee5ffdb885e1555d
tanto <tantows001@gmail.com>
2023-10-20 19:05:04 +0700

    final commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('/build/assets/app-735a6205.css') }}">
 </head>
 <body>
     @include('components.navbar')
 
     @yield('content')
+
+    <script src="{{ asset('/build/assets/app-422dbcc3.js ') }}"></script>
 </body>
 </html>
